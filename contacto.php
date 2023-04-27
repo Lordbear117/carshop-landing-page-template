7f5148af30e83e3cb7767560ae5294447b7dd4b4
Cesar.castrop <cesar.castrop@coppel.com>
2023-04-26 23:28:54 -0700

    First Commit

---
+++ contacto.php
@@ -0,0 +1,290 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Taller Celymel</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/nivo-slider/css/nivo-slider.css" rel="stylesheet">
+  <link href="lib/owlcarousel/owl.carousel.css" rel="stylesheet">
+  <link href="lib/owlcarousel/owl.transitions.css" rel="stylesheet">
+ <!-- <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet"> -->
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+  <!-- Nivo Slider Them
+    e -->
+  <link href="css/nivo-slider-theme.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- Responsive Stylesheet File -->
+  <link href="css/responsive.css" rel="stylesheet">
+
+</head>
+
+<body data-spy="scroll" data-target="#navbar-example">
+
+  <div id="preloader"></div>
+
+  <header>
+    
+    <div id="sticker" class="header-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12">
+
+       
+            <nav class="navbar navbar-default">
+       
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".bs-example-navbar-collapse-1" aria-expanded="false">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+               
+                <a class="navbar-brand page-scroll sticky-logo" href="index.html">
+                  <h1><span>Taller</span>Celymel</h1>
+                  <!-- <img src="img/logo.png" alt="" title=""> -->
+								</a>
+              </div>
+              <div class="collapse navbar-collapse main-menu bs-example-navbar-collapse-1" id="navbar-example">
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="active">
+                    <a class="page-scroll" href="index.html">Home</a>
+                  </li>
+                  
+                  <li>
+                    <a class="page-scroll" href="blog.html">Contacto</a>
+                  </li>
+                </ul>
+              </div>
+              
+            </nav>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+   
+  </header>
+  
+  <div class="header-bg page-area">
+    <div class="home-overly"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="slider-content text-center">
+            <div class="header-bottom">
+              <div class="layer2 wow zoomIn" data-wow-duration="1s" data-wow-delay=".4s">
+                <h1 class="title2">Contacto</h1>
+              </div>
+              <div class="layer3 wow zoomInUp" data-wow-duration="2s" data-wow-delay="1s">
+                <h2 class="title3">Contactanos para más información</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+  
+     <div id="citas" class="about-area area-padding">
+          <div class="container">    
+            <form action="" method="post" role="form" class="contactForm">
+      <div id="contact" class="contact-area">
+    <div class="contact-inner area-padding">
+      <div class="contact-overly"></div>
+      <div class="container ">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="section-headline text-center">
+              <h2>Contactanos</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="contact-icon text-center">
+              <div class="single-icon">
+                <i class="fa fa-mobile"></i>
+                <p>
+                  Teléfono: +52 6677 55488 55<br>
+                  <span>Lunes-Sábado (7am-5pm)</span>
+                </p>
+              </div>
+            </div>
+          </div>
+   
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="contact-icon text-center">
+              <div class="single-icon">
+                <i class="fa fa-envelope-o"></i>
+                <p>
+
+                  Correo: info@celymel.com<br>
+                  <span>Web:</span>
+                  <a href="http://tallercelymel.dx.am/index.html">www.tallercelymel.com</a>
+                </p>
+              </div>
+            </div>
+          </div>
+     
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="contact-icon text-center">
+              <div class="single-icon">
+                <i class="fa fa-map-marker"></i>
+                <p>
+                  Ubicación: 1681 Río Sta. María y o Mutualismo<br>
+                  <span>Sinaloa, México</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+
+
+         
+          <div class="col-md-6 col-sm-6 col-xs-12">
+          
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.112977239388!2d-107.41414098547784!3d24.791584654121312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd18861fa6f89%3A0x7a0e27a8f01d9928!2sTaller%20Mec%C3%A1nico%20Celymel!5e0!3m2!1ses!2smx!4v1575504944588!5m2!1ses!2smx" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+          
+          </div>
+       
+
+          <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="form contact-form">
+              <form action="" method="post" role="form" class="contactForm">
+                <div class="form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Tu nombre" required />
+                  
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Tu correo" required />
+                  
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Asunto" required />
+              
+                </div>
+                <div class="form-group">
+                  <textarea class="form-control" name="message" rows="5" placeholder="Mensaje" required></textarea>
+                 
+                </div>
+                <div class="text-center"><button type="submit">Enviar mensaje</button></div>
+              </form>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
+</div>
+
+  
+</div>
+</div>
+</div>
+
+  <footer>
+    <div class="footer-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="footer-content">
+              <div class="footer-head">
+                <div class="footer-logo">
+                  <h2><span>Taller</span>Celymel</h2>
+                </div>
+
+                <p>En nuestro taller ofrecemos el mejor servicio y la mejor calidad garantizada.</p>
+                <div class="footer-icons">
+                  <ul>
+                    <li>
+                      <a href="https://www.facebook.com/mecanicaculiacan/"><i class="fab fa-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fab fa-google"></i></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+       
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="footer-content">
+              <div class="footer-head">
+                <h4>Información</h4>
+                <p>
+                  Contactanos por tu medio preferido.
+                </p>
+                <div class="footer-contacts">
+                  <p><span>Tel:</span> +52 6677 55488 55</p>
+                  <p><span>Correo:</span> info@celymel.com</p>
+                  <p><span>Horario:</span> 7am-5pm</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+         
+        </div>
+      </div>
+    </div>
+    <div class="footer-area-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="copyright text-center">
+              <p>
+                &copy; Copyright <strong>TallerCelymel</strong>. All Rights Reserved
+              </p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/parallax/parallax.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+  <script src="lib/appear/jquery.appear.js"></script>
+  <script src="https://kit.fontawesome.com/8cb973f305.js" crossorigin="anonymous"></script>
+  <script src="lib/wow/wow.min.js"></script>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
